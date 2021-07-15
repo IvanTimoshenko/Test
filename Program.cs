76607744c5b79d3431e20d4ae3d5377ab9ce2a4d
Ivan Timoshenko <ivantimoshenkoreserve@yandex.ru>
2021-07-15 10:07:34 +0300

    first message

--- Program.cs
+++ Program.cs
@@ -4,9 +4,11 @@ namespace GitProject
 {
     class Program
     {
+        Messages messages = new Messages();
         static void Main(string[] args)
         {
             Console.WriteLine("Hi");
+            Console.WriteLine(Messages.FirstMessage());
         }
     }
 }
