6592ea650eb06036457ebfcbd0e70f2db5bc1b43
Ivan Timoshenko <ivantimoshenkoreserve@yandex.ru>
2021-07-15 15:11:25 +0300

    added color (green) for text in console

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace GitProject
         static void Main(string[] args)
         {
             Messages messages = new Messages();
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Hello World!");
             messages.FirstMessage();
             Console.ReadKey();
