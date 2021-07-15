a92c0e66b38a1ad1df789a35135247a0d6883e7e
Ivan Timoshenko <ivantimoshenkoreserve@yandex.ru>
2021-07-15 11:01:29 +0300

    new comment

--- Messages.cs
+++ Messages.cs
@@ -4,7 +4,7 @@ namespace GitProject
     public class Messages
     {
         public string FirstMessage(){
-            Console.WriteLine("put your message here:");
+            Console.WriteLine("put your message here:"); //new comment
             return Console.ReadLine();
         }
     }
